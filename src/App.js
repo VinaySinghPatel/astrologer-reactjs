81dea68e8bc907a8f3c2acc49c4d071f3b2b8f6e
Vinay Singh Patel <vinaypatel898944@gmail.com>
2025-08-10 11:18:01 +0530

    first commitn

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
 import './App.css';
+import Navbar from './component/Navbar';
+import Secondpart from './component/second-part';
+import Mainimage from './component/mainimage';
+import ClearDoubt from './component/div-for-clear-doubt';
+import LiveGuruCards from './component/live-guru-cards';
+import FeaturedGuruCards from './component/featured-guru-cards';
+import WorkAnddetail from './component/how-it-work';
+import Testinomials from './component/testinomials';
+import Ourblogs from './component/our-blogs';
+import GuideToOurFeature from './component/guide-our-feature';
+import Footer from './component/footer';
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      offset: 100
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+      <Secondpart/>
+      <Mainimage/>
+      <ClearDoubt/>
+      <LiveGuruCards/>
+      <FeaturedGuruCards/>
+      <WorkAnddetail/>
+      <Testinomials/>
+      <Ourblogs/>
+      <GuideToOurFeature/>
+      <Footer/>
+    </>
   );
 }
 
